1f8c4e52486de0d93d71e9023f7661a54b318121
Brooke Bullek <btb004@bucknell.edu>
2016-04-18 23:19:39 -0400

    Main class works with MVC design pattern now; just run TetrisMain.java

--- src/tetris/main/TetrisMain.java
+++ src/tetris/main/TetrisMain.java
@@ -5,12 +5,46 @@
  */
 package tetris.main;
 
+import java.io.File;
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.SlickException;
+import tetris.controller.MainController;
+import tetris.model.MainModel;
+import tetris.view.MainView;
+import tetris.view.Window;
+
 /**
+ * Contains the necessary methods to launch and run the Tetris application
  *
- * @author dtv002
+ * @author Xizhou Li
  */
 public class TetrisMain {
-    public static void main() {
+    public static boolean _APPLET = true;
 
+    /**
+     * Main method to run the Tetris application
+     *
+     * @param args Command line arguments
+     * @author Xizhou Li
+     */
+    public static void main(String[] args) {
+        _APPLET = false;
+        File file = new File("natives");
+        if (file.exists()) {
+            System.setProperty("org.lwjgl.librarypath", file.getAbsolutePath());
+        }
+        try {
+            // instantiate the model, view & controller
+            MainModel theModel = new MainModel();
+            MainView theView = new MainView("Tetris", null);
+            MainController theController = new MainController(theModel, theView);
+            theView.setMainController(theController);
+
+            AppGameContainer game = new AppGameContainer(theView);
+            game.setDisplayMode(Window.getWIDTH(), Window.getHEIGHT(), false);
+            game.start();
+        } catch (SlickException ex) {
+            ex.printStackTrace();
+        }
     }
 }
