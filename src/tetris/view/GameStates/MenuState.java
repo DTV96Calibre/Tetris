b3835276f722ac767778bdd2331d900172f72f43
Andre Amirsaleh <aa026@bucknell.edu>
2016-04-27 13:19:53 -0400

    up to date now?

--- src/tetris/view/GameStates/MenuState.java
+++ src/tetris/view/GameStates/MenuState.java
@@ -5,10 +5,8 @@
  */
 package tetris.view.GameStates;
 
-import org.lwjgl.input.Mouse;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
-import org.newdawn.slick.Image;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
@@ -18,19 +16,8 @@ import tetris.resources.Resources;
 /**
  *
  * @author xizhouli
- * @see https://www.youtube.com/watch?v=EZcQe2EMJIs
  */
 public class MenuState extends BasicGameState {
-
-    /**
-     * The button to start a new game
-     */
-    Image playNow;
-
-    /**
-     * The button to exit the program
-     */
-    Image exitGame;
 
     public MenuState(int state) {
     }
@@ -42,28 +29,16 @@ public class MenuState extends BasicGameState {
 
     @Override
     public void init(GameContainer gc, StateBasedGame s) throws SlickException {
-        playNow = new Image("playImage.png");
-        exitGame = new Image("exitImage");
     }
 
     @Override
     public void render(GameContainer gc, StateBasedGame s, Graphics g) throws SlickException {
-        g.drawString("Welcome to Tetris!", 100, 50);
-        playNow.draw(100, 100);
-        exitGame.draw(100, 300);
         Resources.getImages().get("menu").draw(200, 100, 240, 160);
         Resources.getImages().get("play").draw(223, 300, 194, 200);
     }
 
     @Override
     public void update(GameContainer gc, StateBasedGame s, int delta) throws SlickException {
-        int posX = Mouse.getX();
-        int posY = Mouse.getY();
-        if ((posX > 100 && posX < 600) && (posY > 100 && posY < 600)) {
-            if (Mouse.isButtonDown(0)) {
-                s.enterState(1);
-            }
-        }
         if (gc.getInput().isKeyPressed(Input.KEY_ENTER)) {
             s.enterState(state.GAME);
         }
