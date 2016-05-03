65bbd7bda03ad39ffbbf6db115ec43609a8b0e33
Brooke Bullek <btb004@bucknell.edu>
2016-05-02 23:15:38 -0400

    Added more JUnit tests

--- test/tetris/model/MainModelTest.java
+++ test/tetris/model/MainModelTest.java
@@ -16,8 +16,10 @@
 package tetris.model;
 
 import java.awt.Point;
+import java.util.ArrayList;
 import org.junit.After;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -40,66 +42,41 @@ public class MainModelTest {
 
     /**
      * Test of initializeBag method, of class MainModel.
+     *
+     * @author Brooke Bullek
      */
     @Test
     public void testInitializeBag() {
         System.out.println("initializeBag");
-        MainModel instance = new MainModel();
-        instance.initializeBag();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        MainModel model = new MainModel();
+        ArrayList<TShape> instance = model.getBag();
+
+        // verify each TShape is present within the bag
+        for (TShape shape : TShape.values()) {
+            assertTrue(instance.contains(shape));
+        }
     }
 
     /**
      * Test of lockActiveTetrimino method, of class MainModel.
+     *
+     * @author Brooke Bullek
      */
     @Test
     public void testLockActiveTetrimino() {
         System.out.println("lockActiveTetrimino");
         MainModel instance = new MainModel();
+        instance.setActiveTetrimino(TShape.I_BLOCK);
+        instance.instantDropTetrimino();
         instance.lockActiveTetrimino();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
 
-    /**
-     * Test of increaseDifficulty method, of class MainModel.
-     */
-    @Test
-    public void testIncreaseDifficulty() {
-        System.out.println("increaseDifficulty");
-        int lines = 0;
-        MainModel instance = new MainModel();
-        instance.increaseDifficulty(lines);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of holdActiveTetrimino method, of class MainModel.
-     */
-    @Test
-    public void testHoldActiveTetrimino() {
-        System.out.println("holdActiveTetrimino");
-        MainModel instance = new MainModel();
-        instance.holdActiveTetrimino();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of calculateNewBlockPositions method, of class MainModel.
-     */
-    @Test
-    public void testCalculateNewBlockPositions() {
-        System.out.println("calculateNewBlockPositions");
-        int shiftAmountX = 0;
-        int shiftAmountY = 0;
-        MainModel instance = new MainModel();
-        Point[] expResult = null;
-        Point[] result = instance.calculateNewBlockPositions(shiftAmountX,
-                                                             shiftAmountY);
-        assertArrayEquals(expResult, result);
+        // verify the I-Block's locked position in the gameboard
+        Point[] positions = {new Point(4, 19), new Point(5, 19), new Point(6, 19), new Point(
+                             7, 19)};
+        for (Point point : positions) {
+            // assert there is a block here
+            assertTrue(instance.getMyBoard().getTheBoard() != null);
+        }
     }
 
     /**
@@ -135,28 +112,70 @@ public class MainModelTest {
 
     /**
      * Test of moveActiveTetrimino method, of class MainModel.
+     *
+     * @author Brooke Bullek & Xizhou Li
      */
     @Test
     public void testMoveActiveTetrimino() {
         System.out.println("moveActiveTetrimino");
-        Direction d = null;
+        Direction d = Direction.DOWN;
         MainModel instance = new MainModel();
-        boolean expResult = false;
-        boolean result = instance.moveActiveTetrimino(d);
+
+        // put blocks in the gameboard so the path is obstructed
+        GameBoard instanceBoard = new GameBoard();
+        for (int i = 0; i < GameBoard.WIDTH; i++) {
+            for (int j = 2; i < GameBoard.HEIGHT; i++) {
+                // all but the first two rows have blocks
+                instanceBoard.setBlock(j, j, new Block("red", new Point(0, 0)));
+            }
+        }
+        instance.setMyBoard(instanceBoard);
+
+        boolean expResult = true;
+        boolean result = instance.moveActiveTetrimino(d); // move down
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+
+        MainModel model = new MainModel();
+        GameBoard board = model.getMyBoard();
+        Tetrimino active = new Tetrimino(TShape.S_BLOCK);
+        model.setActiveTetrimino(active);
+        model.setActiveTetriminoLocation(new Point(1, 0));
+        // If a S-Shape Tetrimino is initialized on the most left side of the board
+        // it would not be able to move left
+        result = model.moveActiveTetrimino(Direction.LEFT);
+        expResult = false;
+        assertEquals(result, expResult);
+
+        model.instantDropTetrimino();
+        active = new Tetrimino(TShape.T_BLOCK);
+        // A new T-Block will not be able to move left if there's a new S-Block on his left.
+        model.setActiveTetrimino(active);
+        model.setActiveTetriminoLocation(new Point(3, 18));
+        expResult = false;
+        result = model.moveActiveTetrimino(Direction.LEFT);
+        assertEquals(result, expResult);
     }
 
     /**
      * Test of instantDropTetrimino method, of class MainModel.
+     *
+     * @author Xizhou Li
      */
     @Test
     public void testInstantDropTetrimino() {
         System.out.println("instantDropTetrimino");
-        MainModel instance = new MainModel();
-        instance.instantDropTetrimino();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        MainModel model = new MainModel();
+        GameBoard board = model.getMyBoard();
+        Tetrimino active = new Tetrimino(TShape.S_BLOCK);
+        model.setActiveTetrimino(active);
+        model.setActiveTetriminoLocation(new Point(1, 0));
+        model.instantDropTetrimino();
+        active = new Tetrimino(TShape.T_BLOCK);
+        // A new T-Block will not be able to move left if there's a new S-Block on his left.
+        model.setActiveTetrimino(active);
+        model.setActiveTetriminoLocation(new Point(3, 18));
+        boolean expResult = false;
+        boolean result = model.moveActiveTetrimino(Direction.LEFT);
+        assertEquals(result, expResult);
     }
 }
